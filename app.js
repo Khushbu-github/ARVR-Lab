42a0b9da2cf15613014c0e808de3919b1d579942
Khushbu-github <khushbuhajari530@gmail.com>
2026-01-21 10:44:42 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// LAB – 4
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Start the server
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
